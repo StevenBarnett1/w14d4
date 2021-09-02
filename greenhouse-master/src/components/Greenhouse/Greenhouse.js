078f78a6b59ace558e0721efbb8b7fba421abdda
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-09-02 10:24:13 -0700

    finished phase 2

--- greenhouse-master/src/components/Greenhouse/Greenhouse.js
+++ greenhouse-master/src/components/Greenhouse/Greenhouse.js
@@ -1,19 +1,29 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
-
+import {useTheme} from "../../context/ThemeContext"
 import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
+
 function Greenhouse() {
+
+  let {themeName} = useTheme()
+  let picType
+  if(themeName === "day"){
+    picType = dayImage
+  } else {
+    picType = nightImage
+  }
   return (
     <section>
-      <img className='greenhouse-img' src={dayImage} alt='greenhouse' />
+      <img className='greenhouse-img' src={picType} alt='greenhouse' />
       <LightSwitch />
       <ClimateStats />
     </section>
   );
 }
+
 
 export default Greenhouse;
