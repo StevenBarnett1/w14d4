162b146090b440cb916e37afc583a4edd20bfd1a
sherryyu <bambooyu715@gmail.com>
2021-09-02 19:02:58 -0400

    stuck on phase 2

--- react-context-star-trek-main/src/context/AppContext.js
+++ react-context-star-trek-main/src/context/AppContext.js
@@ -1,13 +1,27 @@
-import { createContext } from 'react';
-import { initialCards } from "../mockdata/CardData";
+import { createContext, useState } from 'react';
+import { initialCards, initialInventory, initialDecks } from "../mockdata/CardData";
 
 
 export const AppContext = createContext();
 
-
 export const AppContextProvider = ({children}) => {
+    const [inventoryState, setInventoryState] = useState(initialInventory)
+    const [deckState, setdeckState] = useState(initialDecks)
+    // console.log(applicationState[1]);
+    const buyCardForPlayer = (id) => {
+        let initialInventory
+        let playerDeckArr = deckState[0].cards;
+        const cardToAdd = initialCards.find(card => card.id === id);
+        let otherPlayersDeck = deckState[1];
+        setdeckState();
+
+    }
     return (
-        <AppContext.Provider value={{ cards: initialCards}}>
+        <AppContext.Provider value={{
+            cards: initialCards,
+             ...applicationState,
+            buyCard: buyCardForPlayer,
+        }}>
           {children}
         </AppContext.Provider>
       );
